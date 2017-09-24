4e9baa0e2f354a8e0b7b9ed88177f16233f61037
bennyficial <benny.u.lee@gmail.com>
2017-09-23 22:49:52 -0700

    upload

---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Chili Cheeseburger', true),('BBQ Bacon Burger', false),('Avocado Burger', false),('Pastrami burger', true)
---
+++ models/burger.js
@@ -0,0 +1,20 @@
+var orm = require('./../config/orm.js');
+
+var burger = {
+    selectAll: function(cb){
+        orm.selectAll('burgers', function(res){
+            cb(res);
+        })
+    },
+
+    insertOne: function(name,cb){
+        orm.insertOne('burgers', name, cb);
+    },
+
+    updateOne: function(id, cb){
+        orm.updateOne('burgers', id, cb)
+    }
+}
+
+// Export the database functions for the controller (burgers_controller.js).
+module.exports = burger;
